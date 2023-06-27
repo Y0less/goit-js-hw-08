50ac28893b2ddbdf405d1cb8c7643f77958f307a
Oleksandr Stakhurskyi <yoles0@gmail.com>
2023-06-28 00:35:46 +0300

    first task completed

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,33 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
-
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 console.log(galleryItems);
+
+const galleryGrid = document.querySelector('.gallery');
+
+importMarkupInGallery(generateMarkup(galleryItems));
+
+function generateMarkup(galleryObj) {
+  return galleryObj
+    .map(
+      ({ preview, original, description }) =>
+        `<li class="gallery__item">
+   <a class="gallery__link" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+   </a>
+</li>`
+    )
+    .join('');
+}
+
+function importMarkupInGallery(markup) {
+  galleryGrid.innerHTML = markup;
+}
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+  overlayOpacity: 0.85,
+});
