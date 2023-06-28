18553afc61eae6995bbe7f55ed0a850d8ece727e
Oleksandr Stakhurskyi <yoles0@gmail.com>
2023-06-28 17:36:08 +0300

    task2 going on

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,29 @@
+import Player from '@vimeo/player';
+const iframe = document.querySelector('iframe');
+const vimeoPlayer = new Player(iframe);
 
+vimeoPlayer
+  .setCurrentTime(localStorage.getItem('videoplayer-current-time'))
+  .then(function (seconds) {
+    console.log('seconds :>> ', seconds);
+  })
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        console.log(
+          'Saved time for player was less than 0 or greater than the video’s duration'
+        );
+        break;
+
+      default:
+        console.log('Error', error.name);
+        break;
+    }
+  });
+
+vimeoPlayer.on('timeupdate', onPlay);
+
+function onPlay(data) {
+  console.log('data :>> ', data.seconds);
+  localStorage.setItem('videoplayer-current-time', data.seconds);
+}
