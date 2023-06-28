629f3e38ae1fca52ea0beed2e5135fdbf1dad0ea
Oleksandr Stakhurskyi <yoles0@gmail.com>
2023-06-28 14:04:07 +0300

    Update 01-gallery.js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -4,7 +4,6 @@ import { generateMarkup, importMarkupInGallery } from './01-modules';
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 // Change code below this line
-console.log('generateMarkup :>> ', generateMarkup);
 
 importMarkupInGallery(generateMarkup(galleryItems));
 
