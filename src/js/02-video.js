9480a8e8a48c37e464e1aabff4cbe6441c1ad181
Oleksandr Stakhurskyi <yoles0@gmail.com>
2023-06-28 19:20:20 +0300

    task2 test push

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,11 +1,15 @@
 import Player from '@vimeo/player';
 const iframe = document.querySelector('iframe');
 const vimeoPlayer = new Player(iframe);
+const throttle = require('lodash.throttle');
 
+/**
+ * to restore VimeoPlayer current playing time from local storage
+ */
 vimeoPlayer
   .setCurrentTime(localStorage.getItem('videoplayer-current-time'))
   .then(function (seconds) {
-    console.log('seconds :>> ', seconds);
+    console.log('stored player time :>> ', seconds); //comment
   })
   .catch(function (error) {
     switch (error.name) {
@@ -14,16 +18,21 @@ vimeoPlayer
           'Saved time for player was less than 0 or greater than the video’s duration'
         );
         break;
-
       default:
         console.log('Error', error.name);
         break;
     }
   });
 
-vimeoPlayer.on('timeupdate', onPlay);
+/**
+ * to read VimeoPlayer playing time with throtle
+ */
+vimeoPlayer.on('timeupdate', throttle(onPlay, 1000));
 
+/**
+ * to store VimeoPlayer current playing time to local storage
+ */
 function onPlay(data) {
-  console.log('data :>> ', data.seconds);
+  console.log('player time :>> ', data.seconds); //comment
   localStorage.setItem('videoplayer-current-time', data.seconds);
 }
